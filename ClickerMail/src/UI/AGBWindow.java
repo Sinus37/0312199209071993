7447a792d760980e5c00b7d4d9481692d6c36f2a
xxlofoxx <marc.andre.weimer@googlemail.com>
2015-01-27 20:41:29 +0100

    new GUI

--- ClickerMail/src/UI/AGBWindow.java
+++ ClickerMail/src/UI/AGBWindow.java
@@ -7,8 +7,14 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class AGBWindow extends JDialog {
+	
+	boolean agbsAreRead = false;
 
 	private final JPanel contentPanel = new JPanel();
 
@@ -31,25 +37,51 @@ public class AGBWindow extends JDialog {
 	public AGBWindow() {
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JLabel lblRegelnNutzungScheigelaber = new JLabel("Regeln, Nutzung, Scheißgelaber");
+		lblRegelnNutzungScheigelaber.setBounds(31, 42, 370, 16);
+		contentPanel.add(lblRegelnNutzungScheigelaber);
 		{
 			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			buttonPane.setLayout(new FlowLayout(FlowLayout.LEFT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
 				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						setAgbsAreRead(true);
+						System.out.println("AGBs gelesen");
+						setVisible(false);
+						dispose();
+					}
+				});
 				okButton.setActionCommand("OK");
 				buttonPane.add(okButton);
 				getRootPane().setDefaultButton(okButton);
 			}
 			{
 				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						System.out.println("AGB geschlossen ohne zu lesen");
+						dispose();
+					}
+				});
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
 			}
 		}
 	}
 
+	public boolean isAgbsAreRead() {
+		return agbsAreRead;
+	}
+
+	public void setAgbsAreRead(boolean agbsAreRead) {
+		System.out.println("AGB Wert übernommen");
+		this.agbsAreRead = agbsAreRead;
+	}
 }
